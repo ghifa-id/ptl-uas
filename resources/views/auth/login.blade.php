c3b0780f51146422cf8f6f9e6a4afb6c5948c829
Adri Syahrul <adrisyahrul20@gmail.com>
2024-12-19 09:54:16 +0700

    fix login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="w-full max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden rounded-lg">
-        <h1 class="text-4xl font-bold mb-4">Login</h1>
+        <h1 class="text-4xl font-bold mb-4 text-center">Kendaraan Dinas</h1>
         <form method="POST" action="{{ route('authenticate') }}">
             @csrf
             <div class="flex flex-col mb-3">
@@ -10,7 +10,7 @@
                 <div class="flex flex-col">
                     <input id="login" type="text"
                         class="p-4 border border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm block mt-1 w-full text-xl @error('login') border-red-500 shadow-red-500 @enderror"
-                        name="login" value="{{ old('login') }}" placeholder="Username / Email" required>
+                        name="login" value="{{ old('login') }}" placeholder="Nama pengguna / E-mail" required>
                 </div>
             </div>
             <div class="flex flex-col mb-3">
@@ -19,7 +19,7 @@
                 <div class="flex flex-col">
                     <input id="password" type="password"
                         class="p-4 border border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm block mt-1 w-full text-xl @error('login') border-red-500 shadow-red-500 @enderror"
-                        name="password" required placeholder="Password">
+                        name="password" required placeholder="Kata Sandi">
                 </div>
                 @error('login')
                     <span class="text-red-500 text-sm text-center mt-3" role="alert">
@@ -31,7 +31,7 @@
             <div class="flex flex-col gap-3 justify-center items-center">
                 <button type="submit"
                     class="inline-flex w-full justify-center items-center py-4 bg-gray-800 border border-transparent rounded-md font-semibold text-xl text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-50 transition ease-in-out duration-300">
-                    <span class="mr-4 font-bold">Login</span>
+                    <span class="mr-4 font-bold">Masuk</span>
                     <i class="fa fa-arrow-right" aria-hidden="true"></i>
                 </button>
                 <div class="flex gap-1">
